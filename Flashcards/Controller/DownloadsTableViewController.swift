371026ed53430c52b6d4dadf992b2fa1379fefb9
Ralf Ebert <info@ralfebert.de>
2018-11-15 20:00:51 +0100

    12.3. Kartenstapel-Download-Liste mit Beispieldaten

--- Flashcards/Controller/DownloadsTableViewController.swift
+++ Flashcards/Controller/DownloadsTableViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class DownloadsTableViewController: UITableViewController {
 
+    let backend = DemoFlashcardsAPIClient()
+    var allSets = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.allSets = backend.allSets
+    }
+
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1000
+        return allSets.count
     }
 
     override func tableView(_ tableView: UITableView,
@@ -21,9 +29,22 @@ class DownloadsTableViewController: UITableViewController {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "DownloadCell", for: indexPath)
 
-        cell.textLabel?.text = "Zelle \(indexPath.section).\(indexPath.row)"
+        let cellItem = allSets[indexPath.row]
+
+        cell.textLabel?.text = cellItem
         cell.detailTextLabel?.text = "? Lernkarten"
 
         return cell
     }
+
+    // MARK: - Table view delegate
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let set = allSets[indexPath.row]
+        let alert = UIAlertController(title: "Download", message: "Kartenstapel \(set) herunterladen.",
+            preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+    
 }
